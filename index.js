17550274d66ab39a9320e7714182ceb1fdadeba2
Matt Mueller <mattmuelle@gmail.com>
2013-06-08 20:47:13 -0700

    added isurl(url) check

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors);
 
 app.all('/*', function(req, res) {
   var url = req.url.replace(/^\/|\/$/g, '') + '/';
+  if(!isurl(url)) return res.send(400);
   req.pipe(request(url)).pipe(res);
 });
 
@@ -37,6 +38,14 @@ function cors(req, res, next){
   } else {
     return next();
   }
+}
+
+/**
+ * url?
+ */
+
+function isurl(url) {
+  return /^(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?$/i.test(url);
 }
 
 /**
